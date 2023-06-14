7c48968dae2a496a010fc51335c6310c6dab15f1
seifemad <seifemad100@gmail.com>
2023-06-14 21:38:43 +0300

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@ def square_matrix_simple(matrix=[]):
     NewArr = []
     for i in range(matrix):
         for j in range(matrix):
-            NewArr[i,j] = matrix[i,j]**2
+            NewArr.append(matrix[i,j]**2)[i,j]
     return NewArr
